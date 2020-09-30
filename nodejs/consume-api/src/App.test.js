d09a4cb0aedab890db2ce101dfca09716d821e2a
yikth <netyth@gmail.com>
2020-09-30 13:59:46 +0800

    test docker-compose build

--- nodejs/consume-api/src/App.test.js
+++ nodejs/consume-api/src/App.test.js
@@ -2,7 +2,7 @@ import React from 'react';
 import { render } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders author text', () => {
   const { getByText } = render(<App />);
   const linkElement = getByText(/Yik Teng Hie/i);
   expect(linkElement).toBeInTheDocument();
